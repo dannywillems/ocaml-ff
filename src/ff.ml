8d7b9206dc4b5d36caf2d4f01fe635845f8b2cd2
Danny Willems <be.danny.willems@gmail.com>
2020-06-17 16:36:37 +0200

    Add power of unity

--- src/ff.ml
+++ src/ff.ml
@@ -4,8 +4,8 @@ module type T = sig
 
   val order : Z.t
 
-  val size_in_bytes : int
   (** minimal number of bytes required to encode a value of the field. *)
+  val size_in_bytes : int
 
   val zero : unit -> t
 
@@ -37,27 +37,33 @@ module type T = sig
 
   val pow : t -> Z.t -> t
 
-  val of_string : string -> t
   (** Create a value t from a predefined string representation. It is not
       required that to_string of_string t = t. By default, decimal
       representation of the number is used, modulo the order of the field *)
+  val of_string : string -> t
 
-  val to_string : t -> string
   (** String representation of a value t. It is not required that to_string
       of_string t = t. By default, decimal representation of the number is
       used *)
+  val to_string : t -> string
 
-  val of_bytes : Bytes.t -> t
   (** From a predefined bytes representation, construct a value t. It is not
       required that to_bytes of_bytes t = t. By default, little endian encoding
       is used and the given element is modulo the prime order *)
+  val of_bytes : Bytes.t -> t
 
-  val to_bytes : t -> Bytes.t
   (** Convert the value t to a bytes representation which can be used for
       hashing for instance. It is not required that to_bytes of_bytes t = t. By
       default, little endian encoding is used, and length of the resulting bytes
       may vary depending on the order.
   *)
+  val to_bytes : t -> Bytes.t
+
+  (** Returns a nth root of unity *)
+  val get_nth_root_of_unity : Z.t -> t
+
+  (** [is_nth_root_of_unity n x] returns [true] if [x] is a nth-root of unity*)
+  val is_nth_root_of_unity : Z.t -> t -> bool
 end
 
 module MakeFp (S : sig
@@ -130,4 +136,22 @@ end) : T = struct
 
   (* Little endian representation *)
   let to_bytes s = Bytes.of_string (Z.to_bits s)
+
+  let rec get_nth_root_of_unity n =
+    if not (Z.equal (Z.erem (Z.pred order) n) Z.zero) then
+      failwith "n must divide the order of the multiplicate group"
+    else
+      let r = random () in
+      if
+        (not (eq r (zero ())))
+        && eq (pow (pow r (Z.div (Z.pred order) n)) n) (one ())
+      then r
+      else get_nth_root_of_unity n
+
+  let is_nth_root_of_unity n x =
+    if not (Z.equal (Z.erem (Z.pred order) n) Z.zero) then
+      failwith "n must divide the order of the multiplicate group"
+    else
+      (not (eq x (zero ())))
+      && eq (pow (pow x (Z.div (Z.pred order) n)) n) (one ())
 end
