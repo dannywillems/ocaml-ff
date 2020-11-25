06eb93588dd00c9ec07f8fc3c0db0fd6ef00f7b1
Danny Willems <be.danny.willems@gmail.com>
2020-11-25 23:07:40 +0100

    Use new functor MakeAll and add tests for GF(2)

--- test/test_generic_ff.ml
+++ test/test_generic_ff.ml
@@ -1,23 +1,20 @@
+module F2 = Ff.MakeFp (struct
+  let prime_order = Z.of_string "2"
+end)
+
+module F2Tests = Ff_pbt.MakeAll (F2)
+
 module F13 = Ff.MakeFp (struct
   let prime_order = Z.of_string "13"
 end)
 
-module F13ValueGeneration = Ff_pbt.MakeValueGeneration (F13)
-module F13IsZero = Ff_pbt.MakeIsZero (F13)
-module F13Equality = Ff_pbt.MakeEquality (F13)
-module F13FieldProperties = Ff_pbt.MakeFieldProperties (F13)
-module F13MemoryRepresentation = Ff_pbt.MakeMemoryRepresentation (F13)
+module F13Tests = Ff_pbt.MakeAll (F13)
 
 module F1073740201 = Ff.MakeFp (struct
   let prime_order = Z.of_string "1073740201"
 end)
 
-module FF1073740201ValueGeneration = Ff_pbt.MakeValueGeneration (F1073740201)
-module FF1073740201IsZero = Ff_pbt.MakeIsZero (F1073740201)
-module FF1073740201Equality = Ff_pbt.MakeEquality (F1073740201)
-module FF1073740201FieldProperties = Ff_pbt.MakeFieldProperties (F1073740201)
-module FF1073740201MemoryRepresentation =
-  Ff_pbt.MakeMemoryRepresentation (F1073740201)
+module F1073740201Tests = Ff_pbt.MakeAll (F1073740201)
 
 module FFLong = Ff.MakeFp (struct
   let prime_order =
@@ -25,25 +22,14 @@ module FFLong = Ff.MakeFp (struct
       "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787"
 end)
 
-module FFLongValueGeneration = Ff_pbt.MakeValueGeneration (FFLong)
-module FFLongIsZero = Ff_pbt.MakeIsZero (FFLong)
-module FFLongEquality = Ff_pbt.MakeEquality (FFLong)
-module FFLongFieldProperties = Ff_pbt.MakeFieldProperties (FFLong)
-module FFLongMemoryRepresentation = Ff_pbt.MakeMemoryRepresentation (FFLong)
+module FFLongTests = Ff_pbt.MakeAll (FFLong)
 
 (* This is the base field of the Curve 25519, the name comes from its order: p**255 - 19*)
 module FFBaseCurve25519 = Ff.MakeFp (struct
   let prime_order = Z.(pow (succ one) 255 - of_int 19)
 end)
 
-module FFBaseCurve25519ValueGeneration =
-  Ff_pbt.MakeValueGeneration (FFBaseCurve25519)
-module FFBaseCurve25519IsZero = Ff_pbt.MakeIsZero (FFBaseCurve25519)
-module FFBaseCurve25519Equality = Ff_pbt.MakeEquality (FFBaseCurve25519)
-module FFBaseCurve25519FieldProperties =
-  Ff_pbt.MakeFieldProperties (FFBaseCurve25519)
-module FFBaseCurve25519MemoryRepresentation =
-  Ff_pbt.MakeMemoryRepresentation (FFBaseCurve25519)
+module FFBaseCurve25519Tests = Ff_pbt.MakeAll (FFBaseCurve25519)
 
 module F13_2 =
   Ff.MakeFp2
@@ -52,11 +38,7 @@ module F13_2 =
       let nsr = F13.(negate (of_string "2"))
     end)
 
-module F13_2FieldProperties = Ff_pbt.MakeFieldProperties (F13_2)
-module F13_2ValueGeneration = Ff_pbt.MakeValueGeneration (F13_2)
-module F13_2IsZero = Ff_pbt.MakeIsZero (F13_2)
-module F13_2Equality = Ff_pbt.MakeEquality (F13_2)
-module F13_2MemoryRepresentation = Ff_pbt.MakeMemoryRepresentation (F13_2)
+module F13_2Tests = Ff_pbt.MakeAll (F13_2)
 
 let test_size_in_bytes () =
   let open Alcotest in
@@ -85,35 +67,7 @@ let () =
   let open Alcotest in
   run
     "Random fields"
-    [ (* F13 *)
-      F13IsZero.get_tests ();
-      F13ValueGeneration.get_tests ();
-      F13Equality.get_tests ();
-      F13FieldProperties.get_tests ();
-      F13MemoryRepresentation.get_tests ();
-      (* F1073740201 *)
-      FF1073740201IsZero.get_tests ();
-      FF1073740201ValueGeneration.get_tests ();
-      FF1073740201Equality.get_tests ();
-      FF1073740201FieldProperties.get_tests ();
-      FF1073740201MemoryRepresentation.get_tests ();
-      (* FF Long *)
-      FFLongIsZero.get_tests ();
-      FFLongValueGeneration.get_tests ();
-      FFLongEquality.get_tests ();
-      FFLongFieldProperties.get_tests ();
-      FFLongMemoryRepresentation.get_tests ();
-      (* Base field of Curve25519 *)
-      FFBaseCurve25519ValueGeneration.get_tests ();
-      FFBaseCurve25519IsZero.get_tests ();
-      FFBaseCurve25519Equality.get_tests ();
-      FFBaseCurve25519FieldProperties.get_tests ();
-      FFBaseCurve25519MemoryRepresentation.get_tests ();
-      (* GF(13²)*)
-      F13_2FieldProperties.get_tests ();
-      F13_2IsZero.get_tests ();
-      F13_2ValueGeneration.get_tests ();
-      F13_2MemoryRepresentation.get_tests ();
-      F13_2Equality.get_tests ();
-      (* test size in bytes *)
-      test_size_in_bytes () ]
+    ( (test_size_in_bytes () :: F2Tests.get_tests ())
+    @ F13Tests.get_tests () @ FFLongTests.get_tests ()
+    @ FFBaseCurve25519Tests.get_tests ()
+    @ F13_2Tests.get_tests () )
